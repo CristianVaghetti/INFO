9738c0beb161b8a46b86a4fccb903180fb7a218b
CristianVaghetti <vaghetticristian@gmail.com>
2021-12-06 17:41:16 -0300

    Inclusão do exercício Primo_Inferior

---
+++ Funcao_Primo_Inferior.php
@@ -0,0 +1,26 @@
+<?php
+function PrimoInferior($num) {
+    $num--;
+    while (verificaPrimo($num) == False){
+        $num--;
+    }
+    return $num;
+    
+}
+
+function verificaPrimo($num) {
+    $divisivel = 0;
+    for ($cont = 1; $cont <= $num; $cont ++) {
+        if ($num % $cont == 0) {
+            $divisivel ++;
+        }
+    }
+    if($divisivel > 2){
+        return False;
+    }
+    else{
+        return True;
+    }
+}
+
+echo(PrimoInferior(15));
